ab95300679cc2810ed35dd01b18b6395e5551070
exceed96 <exceed96@naver.com>
2024-10-22 17:13:07 +0900

    [Feat] 상대 메세지 디자인 변경

--- src/components/Chat/Message/ReceiveMessage.tsx
+++ src/components/Chat/Message/ReceiveMessage.tsx
@@ -3,9 +3,9 @@ import { TMessage } from "../../../types/MessageTypes";
 export default function ReceiveMessage(props: TMessage) {
   return (
     <div className="flex flex-col">
-      <section className="font-extrabold">{props.nickname}</section>
+      <section className="font-extrabold self-start">{props.nickname}</section>
       <section className="flex gap-2 items-end">
-        <p className="bg-[#ffbbbb] rounded-[8px] py-2 px-2 max-w-[60%] w-fit h-fit break-words whitespace-pre-line text-left">
+        <p className="bg-gray-200 rounded-[8px] py-2 px-2 max-w-[60%] w-fit h-fit break-words whitespace-pre-line text-left border-1px border-black">
           {props.message}
         </p>
         <span className="text-sm">{props.timestamp}</span>
