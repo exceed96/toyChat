8367e1824ee3c560096a86c80e61ee5fb8a78d3a
exceed96 <exceed96@naver.com>
2024-10-22 17:09:17 +0900

    [Feat] 디자인 변경

--- src/components/Chat/ChatInput.tsx
+++ src/components/Chat/ChatInput.tsx
@@ -23,7 +23,7 @@ export default function ChatInput() {
     <form className="w-full flex gap-2" onSubmit={sendMessageHandler}>
       <input
         placeholder="메세지를 입력하세요"
-        className="outline-none w-[80%] font-[HANBatang] pt-2 pb-2 pl-2 rounded-[8px]"
+        className="outline-none w-[80%] font-[HANBatang] pt-2 pb-2 pl-2 rounded-[8px] border-[1px] border-black"
         ref={messageRef}
       ></input>
       <button className="bg-black font-[HANBatang] text-white text-sm w-[20%] rounded-[8px] px-5">
