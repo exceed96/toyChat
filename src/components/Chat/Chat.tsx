1b376888d75c9219fe2cda79e116e26bbf4f1672
exceed96 <exceed96@naver.com>
2024-10-22 17:21:11 +0900

    [Feat] 채팅 디자인 변경

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -3,8 +3,8 @@ import ChatInput from "./ChatInput";
 
 export default function Chat() {
   return (
-    <div className="flex flex-col bg-white w-full h-full xl:w-1/3 xl:h-1/2 p-5 border-[1px] border-black text-center">
-      <h1 className="text-3xl font-[HANBatang]">사 직 서</h1>
+    <div className="flex flex-col bg-gray-300 w-full h-full xl:w-1/3 xl:h-1/2 p-5 border-[1px] border-black text-center">
+      <h1 className="text-3xl font-[Chosunilbo]">사 직 서</h1>
       <ChatShow />
       <ChatInput />
     </div>
