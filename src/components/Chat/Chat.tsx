33d1ec7e2791e4420fe965733f8b20abe4b4cdf5
exceed96 <exceed96@naver.com>
2024-10-22 22:27:31 +0900

    [Feat] pwa 1차 테스트

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -3,8 +3,16 @@ import ChatInput from "./ChatInput";
 
 export default function Chat() {
   return (
-    <div className="flex flex-col bg-gray-100 w-full h-full xl:w-1/3 xl:h-1/2 p-5 border-[1px] border-black text-center">
-      <h1 className="text-3xl font-[Chosunilbo]">사 직 서</h1>
+    <div className="flex flex-col bg-gray-100 w-full h-full xl:w-1/3 xl:h-1/2 p-5 border-[1px] border-black text-center relative">
+      <header className="flex justify-center items-center">
+        <h1 className="text-3xl font-[Chosunilbo] underline underline-offset-4">
+          사 직 서
+        </h1>
+        <img
+          src="/img/chatSetting.svg"
+          className="w-9 h-9 absolute right-5 cursor-pointer"
+        />
+      </header>
       <ChatShow />
       <ChatInput />
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,61 @@
 import { defineConfig } from "vite";
 import tailwindcss from "tailwindcss";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Vite PWA Project",
+        short_name: "Vite PWA Project",
+        display: "standalone",
+        description: "WIM-I 해석 상담 보고서를 위한 검사 어플리케이션",
+        background_color: "#ffffff",
+        theme_color: "#008649",
+        icons: [
+          {
+            src: "img/pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/jpg",
+          },
+          {
+            src: "img/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/pwa/jpg",
+          },
+          {
+            src: "img/pwa-256x256.png",
+            sizes: "256x256",
+            type: "image/jpg",
+            purpose: "any",
+          },
+          {
+            src: "img/pwa-384x384.png",
+            sizes: "256x256",
+            type: "image/jpg",
+            purpose: "any",
+          },
+          {
+            src: "img/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/jpg",
+            purpose: "any",
+          },
+          {
+            src: "img/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   css: {
     postcss: {
       plugins: [tailwindcss()],
