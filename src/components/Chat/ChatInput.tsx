65d5fa0f63612cde4bd4d6a2571121667d6cfa90
exceed96 <exceed96@naver.com>
2024-10-26 17:03:16 +0900

    [Feat] 모바일 키보드..

--- src/components/Chat/ChatInput.tsx
+++ src/components/Chat/ChatInput.tsx
@@ -7,21 +7,28 @@ export default function ChatInput() {
 
   const sendMessageHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     if (!localStorage.getItem("id")) {
       localStorage.setItem("id", nanoid());
     }
+
     const trimmedMessage = messageRef.current?.value.trim();
     if (trimmedMessage) {
       ChatSocket.emit("chatting", {
         nickname: localStorage.getItem("nickname"),
-        message: messageRef.current?.value,
+        message: trimmedMessage,
         sendUserId: localStorage.getItem("id"),
       });
+
       if (messageRef.current) {
         messageRef.current.value = "";
         messageRef.current.focus();
       }
     }
+  };
+
+  const handleButtonClick = () => {
+    messageRef.current?.focus();
   };
 
   return (
@@ -30,8 +37,12 @@ export default function ChatInput() {
         placeholder="메세지를 입력하세요"
         className="outline-none w-[80%] font-[HANBatang] pt-2 pb-2 pl-2 rounded-[8px] border-[1px] border-black"
         ref={messageRef}
-      ></input>
-      <button className="bg-black font-[HANBatang] text-white text-sm w-[20%] rounded-[8px] px-5">
+      />
+      <button
+        className="bg-black font-[HANBatang] text-white text-sm w-[20%] rounded-[8px] px-5"
+        type="submit"
+        onClick={handleButtonClick}
+      >
         전송
       </button>
     </form>
