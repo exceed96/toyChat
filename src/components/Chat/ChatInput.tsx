9c65c5b21674220556670c41b024ed1e80a02cb5
exceed96 <exceed96@naver.com>
2024-10-29 11:26:23 +0900

    [Feat] whitespace 및 전송 에러 수정

--- src/components/Chat/ChatInput.tsx
+++ src/components/Chat/ChatInput.tsx
@@ -3,7 +3,7 @@ import { ChatSocket } from "../../util/ChatSocket";
 import { nanoid } from "nanoid";
 
 export default function ChatInput() {
-  const messageRef = useRef<HTMLInputElement>(null);
+  const messageRef = useRef<HTMLTextAreaElement>(null);
 
   const sendMessageHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -29,12 +29,25 @@ export default function ChatInput() {
     }
   };
 
+  const pressEnterHandler = (e: React.KeyboardEvent<HTMLFormElement>) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      sendMessageHandler(e);
+    }
+  };
+
   return (
-    <form className="w-full flex gap-2" onSubmit={sendMessageHandler}>
-      <input
+    <form
+      className="w-full flex gap-2"
+      onSubmit={sendMessageHandler}
+      onKeyPress={pressEnterHandler}
+    >
+      <textarea
         placeholder="메세지를 입력하세요"
-        className="outline-none w-[80%] font-[HANBatang] pt-2 pb-2 pl-2 rounded-[8px] border-[1px] border-black"
+        className="outline-none w-[80%] font-[HANBatang] pt-2 pb-2 pl-2 rounded-[8px] border-[1px] border-black resize-none"
         ref={messageRef}
+        rows={1}
+        maxLength={500}
       />
       <button
         className="bg-black font-[HANBatang] text-white text-sm w-[20%] rounded-[8px] px-5"
--- src/components/Chat/Message/SendMessage.tsx
+++ src/components/Chat/Message/SendMessage.tsx
@@ -6,7 +6,7 @@ export default function SendMessage(props: TMessage) {
       <section className="font-extrabold">{props.nickname}</section>
       <section className="flex items-end gap-2 max-w-[60%]">
         <span className="text-sm">{props.timestamp}</span>
-        <p className="break-words bg-white rounded-[8px] py-2 px-2 self-end flex-[7] border-[1px] border-gray-200 text-start">
+        <p className="break-words bg-white rounded-[8px] py-2 px-2 self-end flex-[7] border-[1px] border-gray-200 text-start whitespace-pre-line">
           {props.message}
         </p>
       </section>
